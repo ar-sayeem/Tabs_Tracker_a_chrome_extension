ebc984656f3a83dde45d07c17b6a4ade12dbcc3c
ar-sayeem <adnan.rahman.sayeem@gmail.com>
2026-03-27 02:43:21 +0600

    fix: block non-web URLs from being saved via SAVE TAB

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ if (leadsFromLocalStorage) {
 
 tabBtn.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
+    const url = tabs[0].url;
+    if (!url.startsWith("http")) return;
+    myLeads.push(url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
   });
